f0a3348734b9a073e86308dd8e0ab70911e33a9c
David Daso <daso.timileyin@gmail.com>
2022-08-26 16:06:29 +0100

    name spacing

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
+app_name = 'myapp'
 urlpatterns = [
     path('', view=views.index, name='index'),
     path('products/', view=views.Products, name='products'),
