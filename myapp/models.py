6dcc4a336185685d9cd2aac556e1d9653a174b8d
David Daso <daso.timileyin@gmail.com>
2022-09-05 11:58:44 +0100

    images field

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=500)
+    image= models.ImageField(upload_to='images', blank=True)
 
     def __str__(self):
         return self.name
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 import myapp
 
@@ -22,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'))
 ]
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
