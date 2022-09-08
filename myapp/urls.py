3bd07e1c88f761f7db8c0b799a3924aef0a639c7
David Daso <daso.timileyin@gmail.com>
2022-09-08 05:46:05 +0100

    addproduct

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', view=views.index, name='index'),
     path('products/', view=views.Products, name='products'),
     path('product/<int:id>/', view=views.product_detail, name='product'),
-]
+    path('product/add/', view=views.add_product, name='addproduct'),
+
+] 
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.http import HttpResponse
 # Create your views here.
@@ -19,3 +19,15 @@ def product_detail(request, id):
         'product': product
     }
     return render(request, 'detail.html',  context) 
+
+def add_product(request):
+    if request.method == 'POST' :
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        image = request.FILES['upload']
+        product = Product(name=name, price=price, description=description, image=image)
+        product.save()
+        return redirect('/')
+    else:
+        return render(request, 'addproduct.html')
