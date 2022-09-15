905ba18fab07385e4386c326083768b98a6b1e95
DAVID <daso.timileyin@gmail.com>
2022-09-15 12:44:21 +0100

    create profile

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', view=views.register, name='register'),
     path('login/', authentication_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', view=views.profile, name='profile')
-    
+    path('profile/', view=views.profile, name='profile'),
+    path('createprofile/', view=views.create_profile, name='createprofile')
 ] 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 # Create your views here.
 
 def register(request):
@@ -18,4 +19,14 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+@login_required
+def create_profile(request):
+    if request.method == "POST":
+        contact_number = request.POST.get('contact_number')
+        image = request.FILES.get('upload')
+        user = request.user
+        profile = Profile(user=user,contact_number=contact_number, image=image)
+        profile.save()
+    return render(request, 'users/createprofile.html')
 
