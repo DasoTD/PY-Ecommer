1bfe20403ebef5851c607cc0c823e9539bc4226d
DAVID <daso.timileyin@gmail.com>
2022-09-15 11:38:51 +0100

    decorator

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ app_name = 'users'
 urlpatterns = [
     path('register/', view=views.register, name='register'),
     path('login/', authentication_view.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', authentication_view.LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/', authentication_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', view=views.profile, name='profile')
     
 ] 
