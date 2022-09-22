f8d90cbcf981eef34be9252f1672e2cc24ddab82
DAVID <daso.timileyin@gmail.com>
2022-09-22 22:49:57 +0100

    admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields= ('name','price')
 
-admin.site.register(models.Product)
+    def set_price_to_zero(self, request, queryset):
+        queryset.update(price=0)
+        
+    actions = ("set_price_to_zero",)
+
+    list_editable = ('description', 'price')
+
+admin.site.register(models.Product, ProductAdmin)
 admin.site.site_header=("Buy and Sell Website")
 admin.site.site_title=("e-commerce") 
 admin.site.index_title=("manage e-commerce site")
